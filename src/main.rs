a7b98aba89be2efbfd0bfb9182cd5484d9c2f757
Shane Liesegang <shane@techie.net>
2023-01-04 20:35:12 -0500

    built-in works fine, of course

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::process;
+
 use anyhow::{Context, Result};
 use subprocess;
 
-fn main() -> Result<()> {
+fn broken() -> Result<()> {
     let git_status = subprocess::Exec::cmd("git")
         .args(&vec!["status", "--porcelain"])
         .capture()
@@ -10,4 +12,21 @@ fn main() -> Result<()> {
     println!("{:?}", git_status.stdout_str());
 
     Ok(())
+}
+
+fn works() -> Result<()> {
+    let output = process::Command::new("git")
+        .arg("status")
+        .arg("--porcelain")
+        .output()
+        .context("Could not run git status.")?;
+
+    println!("{:?}", std::str::from_utf8(&output.stdout)?);
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    // broken()
+    works()
 }
